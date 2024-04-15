b9006237a87379239665cd8450952f64e42117e1
Burkin Nikita <nik.burk.0403@gmail.com>
2024-04-15 12:31:41 +0300

    Доюавил обработку нажатия на кнопку

--- button.py
+++ button.py
@@ -2,8 +2,10 @@ class Button:
     def __init__(self, center, size) -> None:
         self.center = center
         self.size = size
+        self.hovered = False
 
-    def update(self, mouse_coords) -> None:
+    def update(self, mouse_coords, mouse_state) -> None:
+        self.hovered = False
         if (
             self.center[0] - self.size[0] / 2
             <= mouse_coords[0]
@@ -12,7 +14,10 @@ class Button:
             <= mouse_coords[1]
             <= self.center[1] + self.size[1] / 2
         ):
-            print("mouse in")
+            self.hovered = True
+
+        if mouse_state == "pressed" and self.hovered:
+            print(f"{self} is clicked!")
 
     def render(self) -> None:
         print(f"{self} class <Button> is drawing")
