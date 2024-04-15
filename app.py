b96553c01d247164372aad9bd1eca4c6705e4dde
Burkin Nikita <nik.burk.0403@gmail.com>
2024-04-15 12:13:41 +0300

    Добавил обработку наведения мыши на кнопку

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from button import Button
 class App:
     def __init__(self) -> None:
         self.running = True
-        self.button = Button()
+        self.button = Button([50, 75], [100, 100])
 
     def run(self) -> None:
         while self.running:
-            self.button.update()
+            self.button.update([100, 100])
--- button.py
+++ button.py
@@ -1,9 +1,17 @@
 class Button:
-    def __init__(self) -> None:
-        ...
+    def __init__(self, center, size) -> None:
+        self.center = center
+        self.size = size
 
-    def update(self) -> None:
-        ...
+    def update(self, mouse_coords) -> None:
+        if (
+            self.center[0] - self.size[0] / 2
+            <= mouse_coords[0]
+            <= self.center[0] + self.size[0] / 2
+            and self.center[1] - self.size[1] / 2
+            <= mouse_coords[1]
+            <= self.center[1] + self.size[1] / 2
+        ):
+            print("mouse in")
 
-    def render(self) -> None:
-        ...
+    def render(self) -> None: ...
